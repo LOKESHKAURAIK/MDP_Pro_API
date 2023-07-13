8700c56a8a0066f581f517f872cdc18597022f04
LOKESHKAURAIK <lokeshbpkauraik@gmail.com>
2023-07-14 00:58:52 +0530

    requirements.txt adding

--- mdp_api/mdp_api/settings.py
+++ mdp_api/mdp_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-azyrry$)!3nsdoa9l8jesc1&64awjf^*sc@tbz#vbx_eju_)8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
